3d7c7300d0493b3e392f7a33381ffc6270fcfee4
Rolf <noreply@chess-ix.com>
2017-01-05 19:47:33 +0100

    Code cleanup

--- src/main/java/com/ximedes/sonarqube/SonarApi.java
+++ src/main/java/com/ximedes/sonarqube/SonarApi.java
@@ -6,7 +6,10 @@ import com.google.gson.internal.LinkedTreeMap;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * Abstraction layer for accessing the Sonar Web API.
@@ -22,25 +25,17 @@ public class SonarApi {
 	public List<Project> getProjects() {
 		List<Project> projectArrayList = new ArrayList<>();
 
-		try {
-			List<LinkedTreeMap> projectList = getProjectList();
-			for (int i = 0; i < projectList.size(); i++) {
-				Project project = new Project(
-						this,
-						(String) projectList.get(i).get("id"),
-						(String) projectList.get(i).get("k"),
-						(String) projectList.get(i).get("nm"));
-				projectArrayList.add(project);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
+		List<LinkedTreeMap> projectList = getLinkedTreeMaps("/api/projects/index");
+		for (int i = 0; i < projectList.size(); i++) {
+			Project project = new Project(
+					this,
+					(String) projectList.get(i).get("id"),
+					(String) projectList.get(i).get("k"),
+					(String) projectList.get(i).get("nm"));
+			projectArrayList.add(project);
 		}
 
 		return projectArrayList;
-	}
-
-	private List<LinkedTreeMap> getProjectList() throws IOException {
-		return getLinkedTreeMaps("/api/projects/index");
 	}
 
 	LinkedTreeMap getLinkedTreeMap(String s) {
@@ -57,7 +52,6 @@ public class SonarApi {
 			e.printStackTrace();
 		}
 		return new LinkedTreeMap();
-
 	}
 
 	List<LinkedTreeMap> getLinkedTreeMaps(String s) {
@@ -75,35 +69,4 @@ public class SonarApi {
 		}
 		return Collections.emptyList();
 	}
-
-
-	private Map getProjectLinks(String id) throws IOException {
-
-		URL url = new URL(sonarurl + "/api/project_links/search?projectKey=" + id);
-
-		HashMap<String, String> urls = new HashMap<>();
-
-		try (InputStream content = (InputStream) url.getContent();
-			 // Dirty trick using Scanner to read stream into string.
-			 Scanner scanner = new Scanner(content).useDelimiter("\\a")
-		) {
-			while (scanner.hasNext()) {
-				String rawJson = scanner.next();
-				Gson gson = new Gson();
-
-
-				LinkedTreeMap linkedTreeMap = gson.fromJson(rawJson, LinkedTreeMap.class);
-				List<LinkedTreeMap> links = (List) linkedTreeMap.get("links");
-
-				for (LinkedTreeMap link : links) {
-					String name = (String) link.get("name");
-					String prul = (String) link.get("url");
-					urls.put(name, prul);
-				}
-			}
-		}
-		return urls;
-	}
-
-
 }
